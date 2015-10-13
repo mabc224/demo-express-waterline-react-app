213eb098f8070b6b086323686235a1ad38ddc69d
Arsalan Bilal <mabc224@gmail.com>
2015-10-13 11:17:34 +0500

    fix faker2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var Faker = require('Faker');
+var Faker = require('faker2');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
